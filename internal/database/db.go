62d50a4dc296dafaecb5085a6033bfb3abcbe0c8
CollarKey <mitrikovskyvlad@yandex.ru>
2025-04-14 17:51:33 +0500

    migration updates

--- internal/database/db.go
+++ internal/database/db.go
@@ -11,7 +11,11 @@ var DB *gorm.DB
 func InitDB() {
 	dsn := "host=localhost user=postgres password=yourpassword dbname=postgres port=5432"
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		DisableAutomaticPing:                     true,
+		SkipDefaultTransaction:                   true,
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
 	if err != nil {
 		log.Fatalf("Failed to connect to database, %v ", err)
 	}
