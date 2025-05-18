249f2f8779d2460164a05a45c70622778adeb425
CollarKey <mitrikovskyvlad@yandex.ru>
2025-05-18 19:39:06 +0500

    refactoring PATCH and DELETE handlers: added default structure for PATCH

--- internal/handlers/taskHandlers.go
+++ internal/handlers/taskHandlers.go
@@ -4,15 +4,64 @@ import (
 	"CheckingErrorsHW2/internal/taskService"
 	"CheckingErrorsHW2/internal/web/tasks"
 	"context"
-	"encoding/json"
-	"github.com/gorilla/mux"
+	//"encoding/json"
+	//"fmt"
+	//"github.com/gorilla/mux"
 	"log"
-	"net/http"
-	"strconv"
+	//"net/http"
+	//"strconv"
+	//"golang.org/x/crypto/ssh"
 )
 
 type Handler struct {
 	Service *taskService.TaskService
+}
+
+func (h *Handler) DeleteTasksId(_ context.Context, request tasks.DeleteTasksIdRequestObject) (tasks.DeleteTasksIdResponseObject, error) {
+	id := request.Id
+
+	if id == 0 {
+		return tasks.DeleteTasksId400Response{}, nil
+	}
+
+	err := h.Service.DeleteTask(id)
+	if err != nil {
+		return tasks.DeleteTasksId404Response{}, nil
+	}
+
+	return tasks.DeleteTasksId204Response{}, nil
+}
+
+func (h *Handler) PatchTasksId(_ context.Context, request tasks.PatchTasksIdRequestObject) (tasks.PatchTasksIdResponseObject, error) {
+	id := request.Id
+	taskRequest := request.Body
+
+	errResp400 := tasks.PatchTasksId400Response{}
+	if id == 0 {
+		return errResp400, nil
+	}
+
+	if taskRequest.Task == nil && taskRequest.IsDone == nil {
+		return errResp400, nil
+	}
+
+	taskToUpdate := taskService.Task{
+		Task:   *taskRequest.Task,
+		IsDone: *taskRequest.IsDone,
+	}
+
+	updatedTask, err := h.Service.UpdateTaskByID(uint(id), taskToUpdate)
+	if err != nil {
+		return nil, err
+	}
+
+	response := tasks.PatchTasksId200JSONResponse{
+		Id:     &updatedTask.ID,
+		IsDone: &updatedTask.IsDone,
+		Task:   &updatedTask.Task,
+	}
+
+	return response, nil
 }
 
 func (h *Handler) GetTasks(_ context.Context, _ tasks.GetTasksRequestObject) (tasks.GetTasksResponseObject, error) {
@@ -38,10 +87,23 @@ func (h *Handler) GetTasks(_ context.Context, _ tasks.GetTasksRequestObject) (ta
 func (h *Handler) PostTasks(_ context.Context, request tasks.PostTasksRequestObject) (tasks.PostTasksResponseObject, error) {
 	taskRequest := request.Body
 
-	taskToCreate := taskService.Task{
-		Task:   *taskRequest.Task,
-		IsDone: *taskRequest.IsDone,
+	if request.Body == nil {
+		log.Printf("Invalid request: request body is nil")
+		return nil, nil
 	}
+
+	taskToCreate := taskService.Task{
+		Task:   "",
+		IsDone: false,
+	}
+
+	if request.Body.Task != nil {
+		taskToCreate.Task = *taskRequest.Task
+	}
+	if request.Body.IsDone != nil {
+		taskToCreate.IsDone = *taskRequest.IsDone
+	}
+
 	createdTask, err := h.Service.CreateTask(taskToCreate)
 	if err != nil {
 		return nil, err
@@ -56,63 +118,61 @@ func (h *Handler) PostTasks(_ context.Context, request tasks.PostTasksRequestObj
 	return response, nil
 }
 
-//
-
 func NewHandler(service *taskService.TaskService) *Handler {
 	return &Handler{Service: service}
 }
 
-func (h *Handler) PatchHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	idStr := vars["id"]
-	if idStr == "" {
-		http.Error(w, "Incorrect ID, no ID specified", http.StatusBadRequest)
-		return
-	}
-
-	var task taskService.Task
-	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	id, err := strconv.ParseUint(idStr, 10, 32)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	updatedTask, err := h.Service.UpdateTaskByID(uint(id), task)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(updatedTask); err != nil {
-		log.Printf("Could not encode response %v", err)
-		return
-	}
-}
-
-func (h *Handler) DeleteHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	StrId := vars["id"]
-	if StrId == "" {
-		http.Error(w, "No ID specified", http.StatusBadRequest)
-		return
-	}
-
-	id, err := strconv.ParseUint(StrId, 10, 32)
-	if err != nil {
-		http.Error(w, "Invalid ID format", http.StatusBadRequest)
-		return
-	}
-
-	if err := h.Service.DeleteTask(uint(id)); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusNoContent)
-}
+//func (h *Handler) PatchHandler(w http.ResponseWriter, r *http.Request) {
+//	vars := mux.Vars(r)
+//	idStr := vars["id"]
+//	if idStr == "" {
+//		http.Error(w, "Incorrect ID, no ID specified", http.StatusBadRequest)
+//		return
+//	}
+//
+//	var task taskService.Task
+//	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+//		http.Error(w, err.Error(), http.StatusBadRequest)
+//		return
+//	}
+//
+//	id, err := strconv.ParseUint(idStr, 10, 32)
+//	if err != nil {
+//		http.Error(w, err.Error(), http.StatusBadRequest)
+//		return
+//	}
+//
+//	updatedTask, err := h.Service.UpdateTaskByID(uint(id), task)
+//	if err != nil {
+//		http.Error(w, err.Error(), http.StatusInternalServerError)
+//		return
+//	}
+//
+//	w.Header().Set("Content-Type", "application/json")
+//	if err := json.NewEncoder(w).Encode(updatedTask); err != nil {
+//		log.Printf("Could not encode response %v", err)
+//		return
+//	}
+//}
+//
+//func (h *Handler) DeleteHandler(w http.ResponseWriter, r *http.Request) {
+//	vars := mux.Vars(r)
+//	StrId := vars["id"]
+//	if StrId == "" {
+//		http.Error(w, "No ID specified", http.StatusBadRequest)
+//		return
+//	}
+//
+//	id, err := strconv.ParseUint(StrId, 10, 32)
+//	if err != nil {
+//		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+//		return
+//	}
+//
+//	if err := h.Service.DeleteTask(uint(id)); err != nil {
+//		http.Error(w, err.Error(), http.StatusInternalServerError)
+//		return
+//	}
+//
+//	w.WriteHeader(http.StatusNoContent)
+//}
